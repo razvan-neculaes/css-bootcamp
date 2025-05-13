c1480d621e375ae346ab09685e3fe2628c63cb49
Razvan Neculaes <razvan.neculaes@osf.digital>
2025-05-13 15:24:45 +0300

    Init

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader', // Injects styles into DOM
+          'css-loader',   // Translates CSS into CommonJS
+          'sass-loader',  // Compiles Sass to CSS
+        ],
+      },
+    ],
+  },
+};
