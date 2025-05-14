cbe992290aacf1f4b345deda4608c1a1c3a390a8
Razvan Neculaes <razvan.neculaes@osf.digital>
2025-05-14 17:41:20 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 //import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+// import 'bootstrap';
+
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Poiret One', 'Fredericka the Great']
+  }
+});
 
 import './css/style.css';
